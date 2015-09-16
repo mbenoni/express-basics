2251147f73da9042875419306a04eec993e37d90
Magnus Benoni <magnus@benoni.no>
2015-09-16 15:09:34 +0200

    Integrate static files into app

--- src/app.js
+++ src/app.js
@@ -3,12 +3,20 @@
 var express = require('express'),
       posts = require('./mock/posts.json');
 
+var postLists = Object.keys(posts).map(function(key) {
+  return posts[key];
+});
+
 var app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
 app.get('/', function(req, res) {
+  var path = req.path;
+  res.locals.path = path;
   res.render('index');
 });
 
@@ -16,7 +24,7 @@ app.get('/blog/:title?', function(req, res) {
   var title = req.params.title;
   if (title === undefined) {
       res.status(503);
-      res.send("This page is under construction!");
+      res.render('blog', {posts: postLists});
   } else {
       var post = posts[title] || {} ;
       res.render('post', {post: post});
